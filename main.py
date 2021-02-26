78237f6589d54f5c662bad90b696e3dd0e89d8ce
Yangyu Chen <cyy@cyyself.name>
2021-02-26 23:15:33 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+from time import *
+#CONFIG BEGIN
+authserver = "202.202.0.163"
+# 202.202.0.163 for CQU Campus A and B, 10.254.7.4 for CQU Campus D (Huxi)
+conn = [
+	{
+		"username":"2018xxxx",
+		"password":"233333",#If there is ' in your password, you should manually escape
+		"R6":"0",#mobile flag
+		"bind": {#This is an option for multidial, for single connection, no bind is ok
+			"interface":"eth0",
+			"ip":"172.20.233.66",
+			"gateway":"172.20.233.1"
+		}
+	}
+]
+tmp_file = "/tmp/drcom_result.txt"
+#CONFIG END
+
+def clear_route():
+	os.system("ip route delete {} 1> /dev/null 2> /dev/null".format(authserver))
+
+def switch_route(thisConn):
+	if (not 'bind' in thisConn):
+		return
+	clear_route()
+	interface = thisConn['bind']['interface']
+	ip = thisConn['bind']['ip']
+	gateway = thisConn['bind']['gateway']
+	os.system("ip route add {} via {} src {} dev {}".format(authserver,gateway,ip,interface))
+
+def check_status(username):
+	os.system("curl http://{} 1> {} 2> /dev/null".format(authserver,tmp_file))
+	with open(tmp_file,'r',encoding='ascii',errors='ignore') as file:
+		result = file.read()
+		if result.find("uid='{}'".format(username)) != -1:
+			return True
+	return False
+
+def do_login(username,password,R6="0"):
+	if R6:
+		R6 = "1"
+	else:
+		R6 = "0"
+	os.system("curl -H 'Uip: va5=1.2.3.4.' --resolve 'www.doctorcom.com:443:{}' https://www.doctorcom.com --data '0MKKey=0123456789&R6={}' --data-urlencode 'DDDDD={}' --data-urlencode 'upass={}' -k 1> /dev/null 2> /dev/null".format(authserver,R6,username,password))
+
+def watchdog():
+	while True:
+		for x in conn:
+			switch_route(x)
+			if not check_status(x['username']):
+				print("[INFO] {} attempt login {}".format(asctime(localtime(time())),x['username'],x['R6']))
+				do_login(x['username'],x['password'],x['R6'])
+				if check_status(x['username']):
+					print("[INFO] {} succeed {}".format(asctime(localtime(time())),x['username'],x['R6']))
+				else:
+					print("[ERROR] {} failed {}".format(asctime(localtime(time())),x['username'],x['R6']))
+			clear_route()
+		sleep(300)
+
+watchdog()
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Dr.COM HTTP Multidial
+
+适用于Dr.COM的HTTP登录方式下的监控脚本，且提供了多拨环境下网卡绑定的支持。
+
+本项目的登录方式源于[github.com:Hagb/cqu-duodian-curl](https://github.com/Hagb/cqu-duodian-curl)，感谢该脚本作者提供此idea。
+
+### 所依赖的软件包：
+
+- iproute2
+- curl
+
+### 所需要的Python库：
+
+- time
+- os
+
+以上库均在OpenWRT的软件包的`python3-base`中提供。
+
+### 已经过测试的学校：
+
+- 重庆大学A区
+- 重庆大学虎溪校区
