db2c0df14244c90e9d1c07693247b5254ce1a728
Yangyu Chen <cyy@cyyself.name>
2021-03-02 23:51:39 +0800

    add comment for macvlan

--- examples/cron-macvlan.sh
+++ examples/cron-macvlan.sh
@@ -1,4 +1,5 @@
 # This script is for OpenWRT to create virtual interface
+# First of all, you need to install kmod-macvlan
 # once the master interface restart, the slave interface will be deleted, so use crontab execute once a minute
 */1 * * * * ip link add link eth0.2 dev wan1 type macvlan
 */1 * * * * ip link add link eth0.2 dev wan2 type macvlan
