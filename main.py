2a5f0590a64b248c2e5c3342dbf69bc9bbf8601d
Yangyu Chen <cyy@cyyself.name>
2021-04-08 10:00:25 +0800

    add IPv6 watchdog

--- main.py
+++ main.py
@@ -45,6 +45,17 @@ def check_status(username):
 		return False
 	return False
 
+def check_v6(v6ip):
+	os.system("curl -m 10 http://{} 1> {} 2> /dev/null".format(authserver,tmp_file))
+	try:
+		with open(tmp_file,'r',encoding='ascii',errors='ignore') as file:
+			result = file.read()
+			if result.find("v6ip='{}'".format(v6ip)) != -1:
+				return True
+	except:
+		return False
+	return False
+
 def do_login(username,password,R6="0"):
 	if R6 == "1":
 		R6 = "1"
@@ -53,8 +64,6 @@ def do_login(username,password,R6="0"):
 	os.system("curl -m 10 -H 'Uip: va5=1.2.3.4.' --resolve 'www.doctorcom.com:443:{}' https://www.doctorcom.com --data '0MKKey=0123456789&R6={}' --data-urlencode 'DDDDD={}' --data-urlencode 'upass={}' -k 1> /dev/null 2> /dev/null".format(authserver,R6,username,password))
 
 def do_v6login(thisConn):
-	if (not 'ipv6' in thisConn):
-		return
 	os.system("curl -m 10 \"http://{}/1.htm?mv6={}&url=\" 1> /dev/null 2> /dev/null".format(authserverv6,thisConn['ipv6']))
 
 def write_pid():
@@ -62,6 +71,7 @@ def write_pid():
 		file.write(str(os.getpid()))
 
 def watchdog():
+	cold_start = True
 	while True:
 		for x in conn:
 			if x['night_discon'] and (time()+8*60*60) % (24*60*60) < (6*60*60) and (strftime("%a",gmtime(time()+8*60*60)) not in ['Sat','Sun']):
@@ -70,18 +80,35 @@ def watchdog():
 			if not check_status(x['username']):
 				print("[INFO] {} attempt login {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
 				do_login(x['username'],x['password'],x['R6'])
+				sleep(3)
 				if check_status(x['username']):
 					print("[INFO] {} succeed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-					do_v6login(x)
 				else:
 					print("[ERROR] {} failed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
+					clear_route()
+					continue
+			else:
+				if cold_start:
+					print("[INFO] {} already logged in {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
+			if 'ipv6' in x:
+				if not check_v6(x['ipv6']):
+					do_v6login(x)
+					sleep(3)
+					if check_status(x['ipv6']):
+						print("[INFO] {} IPv6 succeed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
+					else:
+						print("[ERROR] {} IPv6 failed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
+				else:
+					if cold_start:
+						print("[INFO] {} IPv6 already logged in {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
 			clear_route()
 		sleep(120)
+		cold_start = False
 
 def addiprule():
 	os.system("/sbin/ip rule delete to {} lookup main".format(authserver))
 	os.system("/sbin/ip rule add to {} lookup main".format(authserver))# fix unstable problem for mwan3
-	
+
 addiprule()
 write_pid()
 watchdog()
