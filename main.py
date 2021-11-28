cf266659cf0cd27866129eb7f1cb02cd05264d3f
Yangyu Chen <cyy@cyyself.name>
2021-11-28 16:19:01 +0800

    fix unstable in mwan3 and add guide for users

--- main.py
+++ main.py
@@ -3,68 +3,46 @@ import os
 from time import *
 #CONFIG BEGIN
 authserver = "202.202.0.163"
-authserverv6 = "202.202.0.180" # authserver for binding IPv4 and IPv6 Address, 202.202.0.180 is for CQU Campus A
 # 202.202.0.163 for CQU Campus A and B, 10.254.7.4 for CQU Campus D (Huxi)
+# We deprecated IPv6 in this version, you can just add "curl -L -6 $SOME_V6_HTTP_URL" to crontab to keep ipv6 alive
 conn = [
 	{
 		"username":"2018xxxx",
-		"password":"233333",#If there is ' in your password, you should manually escape
-		"R6":"0",#mobile flag
-		"night_discon":False,#disable at weekdays 0:00-6:00 for CQU Huxi
-		"bind": {#This is an option for multidial, for single connection, no bind is ok
-			"interface":"eth0",
-			"ip":"172.20.233.66",
-			"gateway":"172.20.233.1"
-		},
-		"ipv6": "2001:0da8:c800:a000:0000:0000:0000:0000"#Write address with %04x, Optional
+		"password":"233333", 	# If there is ' in your password, you should manually escape
+		"R6":"0", 				# mobile flag
+		"night_discon":False, 	# disable at weekdays 0:00-6:00 for CQU Huxi
+		"interface":"wan0" 		# Warning: this interface value should be interface name as used in mwan3, rather than alias in luci
+	},
+	{
+		"username":"2019xxxx",
+		"password":"233333",
+		"R6":"0",
+		"night_discon":True,
+		"interface":"wan1"
 	}
 ]
 tmp_file = "/tmp/drcom_result.txt"
 #CONFIG END
 
-def clear_route():
-	os.system("ip route delete {} 1> /dev/null 2> /dev/null".format(authserver))
-
-def switch_route(thisConn):
-	if (not 'bind' in thisConn):
-		return
-	clear_route()
-	interface = thisConn['bind']['interface']
-	ip = thisConn['bind']['ip']
-	gateway = thisConn['bind']['gateway']
-	os.system("ip route add {} via {} src {} dev {}".format(authserver,gateway,ip,interface))
-
-def check_status(username):
-	os.system("curl -m 10 http://{} 1> {} 2> /dev/null".format(authserver,tmp_file))
+def check_status(interface):# If you don't use mwan3, you should manually change this function.
+	os.system("/usr/sbin/mwan3 status > {}".format(tmp_file))
 	try:
 		with open(tmp_file,'r',encoding='ascii',errors='ignore') as file:
 			result = file.read()
-			if result.find("uid='{}'".format(username)) != -1:
+			if result.find("interface {} is offline".format(interface)) != -1:
+				return False
+			else:
 				return True
 	except:
-		return False
-	return False
+		return True
 
-def check_v6(v6ip):
-	os.system("curl -m 10 http://{} 1> {} 2> /dev/null".format(authserver,tmp_file))
-	try:
-		with open(tmp_file,'r',encoding='ascii',errors='ignore') as file:
-			result = file.read()
-			if result.find("v6ip='{}'".format(v6ip)) != -1:
-				return True
-	except:
-		return False
-	return False
 
-def do_login(username,password,R6="0"):
+def do_login(username,password,interface,R6="0"):
 	if R6 == "1":
 		R6 = "1"
 	else:
 		R6 = "0"
-	os.system("curl -m 10 -H 'Uip: va5=1.2.3.4.' --resolve 'www.doctorcom.com:443:{}' https://www.doctorcom.com --data '0MKKey=0123456789&R6={}' --data-urlencode 'DDDDD={}' --data-urlencode 'upass={}' -k 1> /dev/null 2> /dev/null".format(authserver,R6,username,password))
-
-def do_v6login(thisConn):
-	os.system("curl -m 10 \"http://{}/1.htm?mv6={}&url=\" 1> /dev/null 2> /dev/null".format(authserverv6,thisConn['ipv6']))
+	os.system("curl -m 10 --interface {} -H 'Uip: va5=1.2.3.4.' --resolve 'www.doctorcom.com:443:{}' https://www.doctorcom.com --data '0MKKey=0123456789&R6={}' --data-urlencode 'DDDDD={}' --data-urlencode 'upass={}' -k 1> /dev/null 2> /dev/null".format(interface,authserver,R6,username,password))
 
 def write_pid():
 	with open('/var/run/drcom.pid','w') as file:
@@ -76,38 +54,19 @@ def watchdog():
 		for x in conn:
 			if x['night_discon'] and (time()+8*60*60) % (24*60*60) < (6*60*60) and (strftime("%a",gmtime(time()+8*60*60)) not in ['Sat','Sun']):
 				continue
-			switch_route(x)
-			if not check_status(x['username']):
+			if not check_status(x['interface']):
 				print("[INFO] {} attempt login {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-				do_login(x['username'],x['password'],x['R6'])
-				sleep(3)
-				if check_status(x['username']):
-					print("[INFO] {} succeed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-				else:
-					print("[ERROR] {} failed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-					clear_route()
-					continue
+				do_login(x['username'],x['password'],x['interface'],x['R6'])
 			else:
 				if cold_start:
 					print("[INFO] {} already logged in {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-			if 'ipv6' in x:
-				if not check_v6(x['ipv6']):
-					do_v6login(x)
-					sleep(3)
-					if check_v6(x['ipv6']):
-						print("[INFO] {} IPv6 succeed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-					else:
-						print("[ERROR] {} IPv6 failed {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-				else:
-					if cold_start:
-						print("[INFO] {} IPv6 already logged in {}:{}".format(asctime(localtime(time())),x['username'],x['R6']))
-			clear_route()
-		sleep(120)
 		cold_start = False
+		sleep(30)
 
 def addiprule():
-	os.system("/sbin/ip rule delete to {} lookup main".format(authserver))
-	os.system("/sbin/ip rule add pref 100 to {} lookup main".format(authserver))# fix unstable problem for mwan3
+	# Avoid error caused by mwan3
+	os.system("/sbin/ip rule del pref 100 to {} lookup main".format(authserver))
+	os.system("/sbin/ip rule add pref 100 to {} lookup main".format(authserver))
 
 addiprule()
 write_pid()
--- readme.md
+++ readme.md
@@ -1,22 +1,72 @@
 # Dr.COM HTTP Multidial
 
-适用于Dr.COM的HTTP登录方式下的监控脚本，且提供了多拨环境下网卡绑定的支持。
+适用于Dr.COM的HTTP登录方式下的监控脚本，且提供了多拨环境下网卡绑定的支持。目前仅支持配合OpenWRT+mwan3使用。
 
 本项目的登录方式源于[github.com:Hagb/cqu-duodian-curl](https://github.com/Hagb/cqu-duodian-curl)，感谢该脚本作者提供此idea。
 
-### 所依赖的软件包：
+## 所依赖的软件包：
 
 - iproute2
 - curl
 
-### 所需要的Python库：
+## 所需要的Python库：
 
 - time
 - os
 
 以上库均在OpenWRT的软件包的`python3-base`中提供。
 
-### 使用方法：
+## 已经过测试的学校：
+
+- 重庆大学A区
+- 重庆大学虎溪校区
+
+## Q&A
+
+1. 为什么删除IPv6支持：
+
+    目前大多数使用Dr.COm认证的学校（如重庆大学），IPv6认证采用访问IPv6 HTTP网页后302到IPv4认证服务器的方式，且在302的地址上包括了本机的IPv6地址，因此我们只需要在crontab中定期执行 `curl -6 -L http://某个IPv6网站` 即可完成IPv6的Keep alive，且目前IPv6没有限速，无需多拨。
+
+2. 如果使用的非OpenWRT，如何使用？
+
+    推荐使用OpenWRT+mwan3进行，若自己使用其他方式进行负载均衡需要在main.py中修改 `check_status()` 函数，使其不从mwan3得到端口的状态。
+
+3. 
+
+## 使用方法：
+
+基于OpenWRT 21.04官方版本。
+
+1. 安装luci-app-mwan3、kmod-macvlan、python3-base、curl
+```shell
+opkg install luci-app-mwan3 kmod-macvlan python3-base curl
+```
+2. 设置单线多拨
+
+在上方菜单进入Network->Interface，然后在Device选项卡中，点击`Add device configuration`，然后给wan口添加多个虚拟接口，如图所示：
+
+![macvlan](images/macvlan.png)
+
+3. 回到Network->Interface的Interface选项卡，添加新的WAN接口，并进行IP、DNS的设置。
+
+![if](images/if.png)
+![ip](images/ip.png)
+
+**这里需要注意的是，每个wan（包括原先自动创建的wan），都必须在Advanced Settings中设置Use Gateway Metric，填写任意数字即可。**
+
+![dns](images/dns.png)
+
+4. 在mwan3中添加接口
+
+在上方菜单进入Network->Load Balancing（若没有找到请在安装luci-app-mwan3后重启路由器），删除原有的配置，然后添加2个我们的物理网卡。
+
+![mwan3](images/mwan3.png)
+
+注意，添加后，需要勾选Enable选项，然后添加几个外网才能访问的IP地址作为Tracking IP，并一定要在最后勾上Flush conntrack table的4个选项。
+
+![mwan3-track](images/mwan3-track.png)
+![mwan3-flush](images/mwan3-flush.png)
+
 
 ```bash
 sudo su
@@ -24,7 +74,58 @@ vi main.py# 根据自己的情况修改config部分
 python3 main.py
 ```
 
-### 已经过测试的学校：
+5. 设置每个端口的Metric和权重
 
-- 重庆大学A区
-- 重庆大学虎溪校区
+Metric表示的是端口使用的优先级，例如，Metric 0的端口存在一个可用，那么就不会去使用Metric大于0的任何端口。如果我们有一个速率较低的上网账号只想在12点断网后使用，可以写Metric 0，并将自己的其它带宽更大的上网账号设置为Metric 1。
+
+而同级Metric中，不同的Weight表示的是分配到这个端口的概率。例如我们分别有20M和40M的上网账号，可以像我这样配置：
+
+![mwan3-member](images/mwan3-members.png)
+
+6. 设置mwan3 policies
+
+这里推荐只使用一个balanced，将所有端口加进来即可，然后Last resort推荐使用default。
+
+![mwan3-policy](images/mwan3-policy.png)
+
+7. 设置mwan3 rules
+
+这里是考虑到部分应用可能存在检测到不同IP地址会出现异常的情况，因此对于https 443端口设置一个sticky，其它都balanced即可。
+
+![mwan3-rules](images/mwan3-rules.png)
+
+8. 在脚本中修改各个用户的配置以及认证服务器，然后传到路由器上（推荐存储为`/usr/drcom.py`目录），并试运行：
+
+![python-config](images/python-config.png)
+
+```shell
+# 传送drcom.py到/usr，新手在Windows下可以通过WinSCP工具。
+cd /usr
+python3 drcom.py
+```
+
+![python-run](images/python-run.png)
+
+成功后按Ctrl+C退出。
+
+9. 将本仓库的`examples/init.d-script.sh`脚本，放置到`/etc/init.d/drcom`，并加上执行权限。
+
+**注意：如果你Python脚本存储的位置并非`/usr/drcom.py`，需要进行相应的调整。**
+
+![init](images/init.png)
+```shell
+# 先将文件传上去，并重命名为drcom（不带后缀）
+cd /etc/init.d
+chmod a+x drcom
+/etc/init.d/drcom enable # 让它开机自启
+/etc/init.d/drcom start  # 启动
+ps | grep drcom          # 查看进程当前运行状态
+```
+![ok](images/ok.png)
+
+
+10. 进入路由器上方菜单的Status->Load Balancing，查看两个端口状态：
+
+![mwan3-status](images/mwan3-status.png)
+
+均为绿色说明成功。
